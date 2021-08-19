90a19fc40fd5d8beb9ef841e2e0aa9c92e0f1a7a
Justin Berger Howes <howes.j.j@gmail.com>
2021-08-19 21:09:39 +1000

    3 tests passing

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+
+def line(arr)
+    new_array = []
+    if arr.length == 0
+        puts "The line is currently empty."
+    else
+        arr.each_with_index do |customer, number_in_line|
+            new_array << "#{number_in_line + 1}. #{customer}"
+
+        end
+        puts 'The line is currently: ' + new_array.join(" ")
+    end
+end
