b96b6e3d4fd362cd4ff460da0c49a5fd7f66bad5
Justin Berger Howes <howes.j.j@gmail.com>
2021-08-20 08:48:35 +1000

    8 tests passing

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,13 @@ def line(arr)
         puts 'The line is currently: ' + new_array.join(" ")
     end
 end
+
+def take_a_number(arr, person)
+    arr.push(person)
+    new_array = []
+    arr.each_with_index do |person_in_line, index|
+        new_array << "Welcome, #{person_in_line}. You are number #{index+1} in line."
+    end
+    puts new_array[-1]
+end
+
